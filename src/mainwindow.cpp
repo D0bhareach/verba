d1a3cd2a9976f366f23cfe7d254ca72c5cf86a52
d0bhareach <viacheslav.modestov@gmail.com>
2024-03-01 09:58:40 +0100

    change main page to QStackedWidget

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,16 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QMainWindow>
-#include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->pushButtonPage1, &QPushButton::clicked, this, &MainWindow::on_pushButtonPage1_clicked);
+    connect(ui->pushButtonPage2, &QPushButton::clicked, this, &MainWindow::on_pushButtonPage2_clicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButtonPage1_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1); // Show Page 2
+}
+
+void MainWindow::on_pushButtonPage2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0); // Show Page 1
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,19 +3,24 @@
 
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButtonPage1_clicked();
+    void on_pushButtonPage2_clicked();
 
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
